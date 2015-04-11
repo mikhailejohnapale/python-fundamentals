b53410fe45d7db5140c6cd8553438fa70a8e3729
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-11 10:52:15 +0800

    tuples and dictionary

--- jan/10-tuples.py
+++ jan/10-tuples.py
@@ -1,1 +1,9 @@
 # 10-tuples.py
+
+a = ('M', 'T', 'W', 'TH', 'F', 'S', 'SU')
+
+print(a[0])
+
+# This should not work
+a.append('Z')
+
---
+++ jan/11-dictionary.py
@@ -0,0 +1,12 @@
+# 11-dictionary.py
+
+
+a = {
+    'name': 'Gwen Stacy',
+    'age': 24,
+    'address': 'New York'
+}
+
+print(a['name'])
+print(a['age'])
+print(a['address'])
