30fb5ae1da1e7af855c2b5a2f7b7a32978e69192
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-11 09:11:13 +0800

    identifiers, reserved words

---
+++ jan/2-identifiers.py
@@ -0,0 +1,19 @@
+# 2-identifiers.py
+
+'''
+Identifiers are used to identify a variable, class
+module, or other object. It should start with A-Z or
+a-z or _ followed by more letters, _'s, 0-9's.
+'''
+
+result = 0
+result_1 = 1
+Result_2 = 2
+_result_ = 3
+_result_10 = 10
+
+print('result', result)
+print('result_1', result_1)
+print('Result_2', Result_2)
+print('_result_', _result_)
+print('_result_10', _result_10)
---
+++ jan/3-reserved-words.py
@@ -0,0 +1,14 @@
+# 3-reserved-words.py
+
+'''
+and         exec        not
+assert      finally     or
+break       for         pass
+class       from        print
+continue    global      raise
+def         if          return
+del         import      try
+elif        in          while
+else        is          with
+except      lambda      yield
+'''
