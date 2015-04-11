507a3d0b9e22bd36dc08007988846d6c3755a1c2
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-11 09:36:21 +0800

    strings

---
+++ jan/7-multiple-assignment.py
@@ -0,0 +1,10 @@
+# 7-multiple-assignment.py
+
+
+a = b = c = 1
+
+print(a, b, c)
+
+x, y, z = 1, 2.0, '3'
+
+print(x, y, z)
---
+++ jan/8-strings.py
@@ -0,0 +1,24 @@
+# 8-strings.py
+
+
+string = 'This is a string'
+
+print(string)
+
+# access first element
+print(string[0])
+
+# access last element
+print(string[-1])
+
+# Prints characters starting from 1st to 5th
+print(strng[:5])
+
+# Prints string starting from 3rd character
+print(string[2:])
+
+# Prints string two times
+print(string[0] * 2)
+
+# Prints concatenated string
+print(string + " 123")
