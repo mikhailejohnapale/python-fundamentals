8e411078cf9d3c15ea0c05a2daef5ddae755edc7
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-11 09:26:36 +0800

    assignment

---
+++ jan/5-multi-line-statements.py
@@ -0,0 +1,8 @@
+# multi-line-statements.py
+
+
+total = 1 + \
+        2 + \
+        3
+
+print('Total', total)
---
+++ jan/6-assignment.py
@@ -0,0 +1,11 @@
+# 6-assignment.py
+
+
+counter = 200  # integer assignment
+meter = 1000.01  # float assignment 
+name = 'Gwen Stacy'  # string assignment
+
+
+print('counter', counter)
+print('meter', meter)
+print('name', name)
