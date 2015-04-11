1a441bff99d75f6548860b806f94159321135051
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-11 09:18:44 +0800

    lines and identation

---
+++ jan/4-lines-identation.py
@@ -0,0 +1,14 @@
+# 4-lines-identation.py
+
+
+if True:
+    # 4 spaces 
+    print('True 1')  # under the if condition
+    if True:
+        # 8 spaces
+        print('True 2')  # under the 2nd if condition
+
+if False:
+    print('False')  # This is the 1st statement of the block
+    print('False')  # This is on the same block
+
