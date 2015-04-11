9306e4e66b98c597303b378d0b5f4ce8e95e3513
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-11 08:23:23 +0800

    comments

---
+++ jan/1-comments.py
@@ -0,0 +1,18 @@
+# 1-comments.py
+
+# This is a single line comment.
+print('This will work!')
+
+# print('This is will not work!')
+
+'''
+This is a multiple line comment.
+This is the second line of the comment.
+This is the third line of the comment.
+'''
+
+"""
+Double quotes also will work on multiple line comment.
+This is the second line of the comment.
+This is the third line of the comment.
+"""
