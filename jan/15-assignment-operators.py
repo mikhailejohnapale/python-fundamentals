ab7a1ac0f5a1d3bdd92bf67b9e3e24d9ae7862e7
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-15 22:44:19 +0800

    assignment operators

---
+++ jan/15-assignment-operators.py
@@ -0,0 +1,25 @@
+# 15-assignment-operators.py
+
+a = 1
+print(a)
+
+a += 2
+print(a)
+
+a -= 1
+print(a)
+
+a *= 3
+print(a)
+
+a /= 2
+print(a)
+
+a %= 2
+print(a)
+
+a **= 3
+print(a)
+
+a //= 2
+print(a)
