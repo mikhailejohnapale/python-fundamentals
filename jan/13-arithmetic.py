fd777692bf26aff39e129a1302d5912209a64e38
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-15 21:39:13 +0800

    arithmetic

---
+++ jan/13-arithmetic.py
@@ -0,0 +1,25 @@
+# 13-arithmetic.py
+
+a = 2
+b = 3
+
+c = a + b
+print(c)
+
+d = a - b
+print(d)
+
+e = a * b
+print(e)
+
+f = a / b
+print(f)
+
+g = a % b
+print(g)
+
+h = a ** b
+print(h)
+
+i = a // b
+print(i)
