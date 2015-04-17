6c387b8b9b1b6db0164d35caaa73efd491e02a3d
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-17 13:12:48 +0800

    for

---
+++ jan/26-for.py
@@ -0,0 +1,20 @@
+# 26-for.py
+
+
+for n in range(10):
+    print(n)
+
+for i in range(1, 10):
+    print(i)
+
+for x in range(1, 11):
+    print(x)
+
+for y in range(10, 0, -1):
+    print(y)
+
+a = ['z', 'a', 1, 2.01]
+
+for e in a:
+    print(e)
+
