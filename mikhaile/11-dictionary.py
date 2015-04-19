247ad9ae35f5bc471cd89ffd944fa90091858810
Mikhaile John Apale <mikhailejohnapale@gmail.com>
2015-04-19 14:41:23 +0800

    dictionary

---
+++ mikhaile/11-dictionary.py
@@ -0,0 +1,13 @@
+
+a = {
+        'name': 'Gwen'  # variable name is a key
+        'age': 24  # variable age is a key
+    }
+
+print(a)
+
+print(a['name'])
+
+a['key'] = 'value'  # add element
+
+print(a)
