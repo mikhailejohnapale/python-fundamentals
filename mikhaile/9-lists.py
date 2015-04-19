09b3af4ef57731186379b7756ac08617db8c2598
Mikhaile John Apale <mikhailejohnapale@gmail.com>
2015-04-19 14:24:48 +0800

    lists

---
+++ mikhaile/9-lists.py
@@ -0,0 +1,18 @@
+
+a = []
+
+a.append(1)  # inserts into the first element
+
+a.append('Two')  # inserts next to the first element
+
+a.append(3.3)  # inserts nest to the second element
+
+print(a)  # prints the whole list
+
+print(a[2])  # prints specific element
+
+a.pop()  # remove(can also specify index) from the last element
+
+a.remove(1)  # remove(index)
+
+print(a)
