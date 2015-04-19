2d4e2be87fb1f1cfb3a2a28fa55e047cf4fd9b69
Mikhaile John Apale <mikhailejohnapale@gmail.com>
2015-04-19 15:39:17 +0800

    loop

---
+++ mikhaile/26-loop.py
@@ -0,0 +1,12 @@
+
+for n in range(10):  # n is a variable and range(10) is an iterable function
+    print(n)
+
+for e in range(1, 11):  # will start at 1 and end at 10
+    print(e)
+
+for x in range(0, 11, 2):  # where the incrementation is 2
+    print(x)
+
+for y in range(10, 0, -1):  # will decrement by 1
+    print(y)
