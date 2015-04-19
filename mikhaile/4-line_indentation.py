c85d6bb7657929ca2259d67bd57a4c03f5e2a4fe
Mikhaile John Apale <mikhailejohnapale@gmail.com>
2015-04-19 10:37:42 +0800

    line indentation

---
+++ mikhaile/4-line_indentation.py
@@ -0,0 +1,12 @@
+
+if True:
+    # 4 spaces = 1 tab
+    print('True1')
+    if True:
+        # 8 space = 2 tab
+        print('Ture2')
+
+
+if False:
+    print(False)    # 1st statement in the block
+    print('False')  # 2nd statement in the block
