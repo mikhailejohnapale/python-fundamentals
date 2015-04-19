db93689f7d156f80ffa5a068add1eabeeb451095
Mikhaile John Apale <mikhailejohnapale@gmail.com>
2015-04-19 10:29:00 +0800

    reserved words

---
+++ mikhaile/3-reserved_words.py
@@ -0,0 +1,13 @@
+
+'''
+and exec not
+assert finally or
+break for pass
+class from print
+continue global raise
+def if return
+del import try
+elif in while
+else is with
+except lambda yield
+'''
