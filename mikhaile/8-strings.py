95daef8b979093554d2db9217573cdb1e2fba658
Mikhaile John Apale <mikhailejohnapale@gmail.com>
2015-04-19 11:06:18 +0800

    strings

---
+++ mikhaile/8-strings.py
@@ -0,0 +1,14 @@
+
+string = 'This is a string'
+
+print(string)
+
+print(string[-1])  # prints last element
+
+print(string[:5])  # prints the first 5 elements
+
+print(string[2:])  # exclude the first 2 and prints the rest
+
+print(string[0]*2)  # prints the first element 2 times
+
+print(string + "123")  # prints the string and appends 123
