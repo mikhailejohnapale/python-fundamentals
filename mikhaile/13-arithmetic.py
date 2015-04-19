346577f7879a8f18ec08f7ebe7af10925ee46e8d
Mikhaile John Apale <mikhailejohnapale@gmail.com>
2015-04-19 14:50:50 +0800

    arithmetic

---
+++ mikhaile/13-arithmetic.py
@@ -0,0 +1,31 @@
+
+a = 2
+b = 3
+
+c = a + b
+
+print(c)
+
+d = a - b
+
+print(d)
+
+e = a * b
+
+print(e)
+
+f = a / b
+
+print(f)
+
+g = a % b
+
+print(g)
+
+h = a ** b
+
+print(h)
+
+i = a // b  # floor division
+
+print(i)
