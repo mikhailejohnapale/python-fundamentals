08eda6b37575ce78a636770380e2008039994ecf
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-20 08:30:41 +0800

    fizzbuzz

---
+++ exercises/1-fizzbuzz.py
@@ -0,0 +1,24 @@
+'''
+Create the program that loops 1 to 100.
+it should display the current number
+if the current number is divisible by 3, print `Fizz`
+if the current number is divisible by 5, print `Buzz`
+if the current number is divisible by 3 and 5, print `FizzBuzz`.
+if the current number is not divisible by 3 and 5, print `FizzyBuzzy`
+'''
+
+'''
+Expected output:
+
+1 FizzyBuzzy
+.
+3 Fizz
+.
+5 Buzz
+.
+.
+15 FizzBuzz
+.
+30 FizzBuzz
+.
+100 Buzz
