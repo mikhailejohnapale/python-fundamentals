84eabe801a5510855098d022757bfa6c9e6dcc9c
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-04-20 08:24:38 +0800

    loops exercise

---
+++ exercises/2-loops.py
@@ -0,0 +1,58 @@
+'''
+sq(x) # function name and parameter
+
+###   # expected output
+###
+###
+
+a = 3 # value of a
+sq(a) # function call
+'''
+
+'''
+tr(x)
+
+#
+##
+###
+
+a = 3
+tr(a)
+'''
+
+'''
+tri(x)
+
+###
+##
+#
+
+a = 3
+tr(a)
+'''
+
+'''
+ar(x)
+
+#
+##
+###
+#
+#
+#
+
+a = 6
+ar(a)
+if a is greater than or equal to 6
+it should display the arrow above
+else it should call sq function
+'''
+
+'''
+Create a script named loops.py
+The program should ask the value of a
+and calls all functions stated above
+it should ask if the user wants to continue or not
+if the input is `y` then it should ask again for the value of a
+and call the functions again in loop
+'''
